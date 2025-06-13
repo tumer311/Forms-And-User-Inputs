794bd4ca1a73fb5b837b35f97dba108cdcfed0be
tumer311 <etumer31148@gmail.com>
2025-06-13 13:11:30 +0300

    forms submission and user input validation project initial commit, already added some solutions

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from './components/Header.jsx';
+// import Signup from './components/SignUp.jsx';
+import Login from './components/StateLogin.jsx';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Login />
+      </main>
+    </>
+  );
+}
+
+export default App;
